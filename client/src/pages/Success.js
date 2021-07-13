c482dffff4df9f376145a07f83dd8854ffb3f9ed
Lacey <lacey.griffith@g.austincc.edu>
2021-07-13 14:50:03 -0500

    checkout functionality, redirect to success then homepage with empty cart

--- client/src/pages/Success.js
+++ client/src/pages/Success.js
@@ -1,11 +1,38 @@
 import React from 'react'
-//import { useEffect } from 'react';
-//import { useMutation } from '@apollo/client';
+import { useEffect } from 'react';
+import { useMutation } from '@apollo/client';
 import Jumbotron from '../components/Jumbotron';
-//import { ADD_ORDER } from '../../src/utils/mutations'
-//import { idbPromise } from '../utils/helpers';
+import { ADD_ORDER } from '../../src/utils/mutations'
+import { idbPromise } from '../utils/helpers';
 
 function Success() {
+    const [addOrder] = useMutation(ADD_ORDER)
+
+    useEffect(() => {
+        async function saveOrder() {
+            //get all items from indexdb cart
+            const cart = await idbPromise('cart','get')
+            //map over them to get array of ids
+            const products = cart.map(item => item._id)
+
+            if(products.length){
+                //save order data
+                const { data } = await addOrder({ variables: { products } })
+                const productData = data.addOrder.products
+
+                //once data is saved, delete items from cart
+                productData.forEach((item )=> {
+                    idbPromise('cart', 'delete', item)
+                })
+
+            }
+        }
+        saveOrder();
+    }, [addOrder])
+
+    //redirect to homepage after 3 seconds
+    setTimeout(function(){window.location.assign('/')}, 3000)
+
     return (
         <div>
             <Jumbotron>
