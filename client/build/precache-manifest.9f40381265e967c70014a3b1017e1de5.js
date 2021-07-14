e766c5958c1116f053797b95ac755f0ae20f7f61
Lacey <lacey.griffith@g.austincc.edu>
2021-07-14 17:49:22 -0500

    create build

---
+++ client/build/precache-manifest.9f40381265e967c70014a3b1017e1de5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26592de9cd5ae3f9298e487cb2eda11f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9a6e31e5885e6f29574",
+    "url": "/static/css/main.ad1af64d.chunk.css"
+  },
+  {
+    "revision": "f0c953ddd2cf3a098cf7",
+    "url": "/static/js/2.e896c90c.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.e896c90c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9a6e31e5885e6f29574",
+    "url": "/static/js/main.6604d5b6.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
